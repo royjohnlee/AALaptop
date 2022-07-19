9160df31f8a6fe562ab195f2b1e83b6e0b70461a
Roy John Lee <royjohnlee@Roys-MBP.attlocal.net>
2022-07-18 19:38:24 -0700

    moving notes around

--- Array and Object/Object.js
+++ Array and Object/Object.js
@@ -1,3 +1,164 @@
+/*
+// ===============
+// === Objects ===
+// ===============
+
+// assigning Key-Value pair
+let testObject = { };  // creating an empty object
+testObject["num"] = 10 // setting "num" as the KEY and 10 as the VALUE of num
+testObject.num2 = 20 // setting num2 as the KEY and 10 as the VALUE
+
+console.log(testObject) // prints: { num: 10, num2: 20 }
+console.log(testObject[0]) // prints: undefined >>> because we did not assign any value to this key
+*/
+
+//=====================================================================//
+
+
+/*
+// ## Both .Dot Notation and []Bracket Notation will allow you to set AND access keys in an object
+    // ## but []Bracket Notation will allow you to use variables as keys when setting a key.
+        // ## This means you don't have to use strings as keys when using Braket Notation
+let obj = {};
+let string = "apples";
+
+obj[string] = "yay!";
+
+console.log(obj)
+*/
+
+//=====================================================================//
+
+/*
+
+// ===========================
+// === ARRAY DESTRUCTURING ===
+// ===========================
+// 1. Destructuring Elements in Order
+
+let array = ["blue", "red", "yellow", "green", "pink"]
+
+//let firstColor = array[0] // old way
+//let secondColor = array[1] // old way
+
+let [firstDestructure, secondDestructure, thirdDestructure] = array;
+
+console.log(firstDestructure) // prints: red
+console.log(secondDestructure) // prints: blue
+console.log(thirdDestructure) // prints: yellow
+*/
+
+
+/*
+// ============================
+// === Object DESTRUCTURING ===
+// ============================
+// 1. Destructuring Keys
+// 2. Aliased Destructuring
+// 3. Nested Destructuring
+
+// 1. Destructuring Keys
+let obj = {name: "Sandy", instruments: ["guitar", "drums"] };
+
+let {name, instruments} = obj // Destructuring syntax
+
+console.log (name) // prints: "Sandy"
+console.log(instruments) // prints: ['guitar', 'drums'] 
+
+//========================================================================//
+
+// 2. Alias destructuring : put the value that the key is associated with TO A NEW variable
+
+let obj2 = {name2: "Randy", instruments2: ["guitar", "drums"] };
+
+let {name2, instruments2: music} = obj2 // instruments2 will now have the variable music
+    // *note that this is still destructing; just assigning instruments2(key) to the variable music
+
+
+console.log (name2) // prints: "Sandy"
+console.log(music) // prints: ['guitar', 'drums'] 
+
+//========================================================================//
+
+// 3. Nested Destructuring
+
+let zooAnimals = {
+    animalID: 12,
+    species: "hippo",
+    favoriteFood: "watermelon",
+    fullName: {
+        fname: "Fiona",
+        lname: "TheCuteHippo"
+    }
+};
+
+let {fullName: {fname}} = zooAnimals 
+// ## BREAK DOWN: go into the "fullName" Key and find the value(fname,lname)
+    // ## once you're in there, grab the what you want (fname)
+    // ## Then, have all that equal to the outter object name (zooAnimals)
+
+// Another Nested Destructuring example:
+
+let house = {
+    numberOfRooms: 3,
+    place:{
+        city: "La Habra",
+        state: "California",
+        zip: 90631
+    },
+    numberOfMembers: 3,
+    color: "white",
+    neighbor1:{
+        n1firstName: "Adam",
+        n1lastName: "Dang"
+    } ,
+    neighbor2:{
+        n2firstName: "Fred",
+        n2middleName: "Derf",
+        n2lastName: "Sohn"
+    },
+    owner:{
+        firstName:"Roy",
+        lastName:"Lee"
+    }
+};
+
+// Access the owner's first name, 
+    //neighbor1's first name and last name, 
+    //neighbor3's first, middle, and last name, 
+    //and set Key of place to a Variable.
+
+
+let {owner:{firstName}} = house
+console.log(firstName) // prints: Roy
+
+let {neighbor1:{n1firstName, n1lastName}} = house  
+console.log(n1firstName,n1lastName) // prints: Adam Dang
+
+let {neighbor2:{n2firstName, n2middleName, n2lastName}} = house 
+console.log(n2firstName,n2middleName, n2lastName) // prints: Fred Derf Sohn
+
+let {place: location} = house // setting a key: place to a variable: location
+console.log(location) // prints: { city: 'La Habra', state: 'California', zip: 90631 }
+
+
+//====================================================================//
+// 1. Destructuring in Function Parameters
+
+let parrot = {name: "Layla", likes: "eat", numberOfToys: 1000}
+
+let sayHelloName = ({name}) => {  // let sayHelloName = ({key}) => {...} 
+    console.log("Hello to " + name)
+}
+
+sayHelloName(parrot) // sayHelloName(Object variable)
+// prints: Hello to Layla
+
+
+//=================================================================================================
+
+*/
+
 // =============================
 // === Object Key-Value Pair ===
 // =============================
